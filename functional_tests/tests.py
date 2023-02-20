bfc49f415c09e1738604ef16ef978d5d7e163686
Jxkeorton <jakeorton99@gmail.com>
2023-02-20 09:54:42 +0000

    bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,12 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     """
     Tests are organised into classes, which inherit from unittest.TestCase.
@@ -141,23 +141,15 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
 
-        # She notices the input box is nicely centered 
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width']/2, 
-            512, 
-            delta=10
-        )
-
         # She starts a new list and sees the input is nicely 
         # centered there too 
-        inputbox.send.keys('testing')
-        inputbox.send.keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
             delta=10
         )
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
     
